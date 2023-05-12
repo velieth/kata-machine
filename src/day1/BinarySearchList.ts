babecaef817be2084c59e1c305a0863ceab80055
Mikhel Patel <mikhel@zeroomega.com>
2023-05-12 15:30:24 -0400

    crystalball

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,17 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let lo = 0;
+    let hi = haystack.length;
 
+    do {
+        let m = Math.floor((lo + (hi - lo) / 2));
+        let v = haystack[m];
+        if(v === needle) {
+            return true;
+        } else if(v < needle) {
+            lo = m + 1;
+        } else {
+            hi = m;
+        }
+    } while(lo < hi);
+    return false;
 }
