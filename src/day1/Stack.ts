2d9eed220ec9a98e967d135d8fd96e3c18e484d8
Mikhel Patel <mikhel@zeroomega.com>
2023-05-15 20:51:58 -0400

    linked-list

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,42 @@
+type Node<T> = {
+    value: T,
+    next?: Node<T>,
+}
+
 export default class Stack<T> {
     public length: number;
-
+    private head?: Node<T>;
+    private tail?: Node<T>;
     
 
     constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
     }
 
     push(item: T): void {
+        const node = {value: item} as Node<T>;
+        this.length++;
+        if(!this.head) {
+            this.head = this.tail = node;
+            return;
+        }
 
+        this.head.next = node;
+        this.head = node;
 }
     pop(): T | undefined {
+        if(!this.head) {
+            return undefined;
+        }
 
+        this.length--;
+        const head = this.head;
+        this.head = this.head.next;
+
+        return head.value;
 }
     peek(): T | undefined {
-
+        return this.head?.value;
 }
 }
