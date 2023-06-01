7024f91e59a745d7aaffa03163e90a25274b70c7
Mikhel Patel <mikhel@zeroomega.com>
2023-06-01 10:30:12 -0400

    comparebt

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,18 @@
 export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+    //base case 1
+    if (a === null && b === null) {
+        return true;
+    }
 
+    //base case 2
+    if(a === null || b === null) {
+        return false;
+    }
+
+    //base case 3
+    if(a.value !== b.value) {
+        return false;
+    }
+
+    return compare(a.left, b.left) && compare(a.right, b.right);
 }
